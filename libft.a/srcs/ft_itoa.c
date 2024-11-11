d29bf1f22efc071c2451bbf49e2b2c61245d2eca
guilhermeurbn <guisanto@student.42lisboa.com>
2024-11-11 13:44:53 +0000

    Update ft_itoa.c

--- libft.a/srcs/ft_itoa.c
+++ libft.a/srcs/ft_itoa.c
@@ -12,25 +12,55 @@
 
 #include "libft.h"
 
-char *ft_itoa(int n)
+static int	len(int n)
 {
-    char    *temp;
-    char    *original;
-    
-    if (n < 0)
-        n = n * -1;
-    while (n > 10)
-    {
-        temp = ft_itoa(n % 10);s
-        
-        original = temp;
-    }
-    return (original);
-}
-int main()
-{
-    int a = 123;
+	int	i;
 
-    printf("%s", ft_itoa(a));
-    return (0);
+	i = 0;
+	if (n == 0)
+		i++;
+	if (n < 0)
+	{
+		n *= -1;
+		i++;
+	}
+	while (n != 0)
+	{
+		n = (n / 10);
+		i++;
+	}
+	return (i);
 }
+
+char	*ft_itoa(int n)
+{
+	int		n_len;
+	long	n_long;
+	char	*dest;
+
+	n_long = n;
+	n_len = len(n_long);
+	dest = malloc(sizeof(char) * (n_len + 1));
+	if (!dest)
+		return (NULL);
+	if (n_long < 0)
+	{
+		n_long *= -1;
+		dest[0] = '-';
+	}
+	if (n_long == 0)
+		dest[n_len - 1] = '0';
+	dest[n_len] = '\0';
+	while (n_long != 0)
+	{
+		dest[n_len - 1] = (n_long % 10) + '0';
+		n_len--;
+		n_long = n_long / 10;
+	}
+	return (dest);
+}
+/*int main()
+{
+	printf("%s\n", ft_itoa(-1));
+	return (0);
+}*/
